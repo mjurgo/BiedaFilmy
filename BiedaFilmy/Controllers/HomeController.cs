bd663c9b060aed2ac663960e5a37ee65d7c7433a
Michał Jurgo <mpjurgo@gmail.com>
2023-05-28 14:40:23 +0200

    Add user roles

--- BiedaFilmy/Controllers/HomeController.cs
+++ BiedaFilmy/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BiedaFilmy.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -18,6 +19,7 @@ namespace BiedaFilmy.Controllers
             return View();
         }
 
+        [Authorize(Roles = "Admin")]
         public IActionResult Privacy()
         {
             return View();
---
+++ BiedaFilmy/Data/Migrations/20230528114850_AddUserRolesMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiedaFilmy.Data.Migrations
+{
+    public partial class AddUserRolesMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { Guid.NewGuid().ToString(), "User", "USER" },
+                    { Guid.NewGuid().ToString(), "Editor", "EDITOR" },
+                    { Guid.NewGuid().ToString(), "Admin", "ADMIN" },
+                }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumns: new[] { "Name", "NormalizedName" },
+                keyValues: new object[,]
+                {
+                    { "User", "USER" },
+                    { "Editor", "EDITOR" },
+                    { "Admin", "ADMIN" },
+                }
+            );
+        }
+    }
+}
