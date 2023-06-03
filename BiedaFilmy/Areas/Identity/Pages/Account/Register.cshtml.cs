4f18ddb1b0717071575863a138ba58af34a49aa9
Michał Jurgo <mpjurgo@gmail.com>
2023-06-03 16:30:06 +0200

    Add admin user

--- BiedaFilmy/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BiedaFilmy/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -133,7 +133,14 @@ namespace BiedaFilmy.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    await _userManager.AddToRoleAsync(user, "User");
+                    if (user.UserName == "skrzetuski")
+                    {
+                        await _userManager.AddToRoleAsync(user, "Admin");
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user, "User");
+                    }
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
